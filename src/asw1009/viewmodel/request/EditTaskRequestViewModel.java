9d8c8876894b2d288ce03cb911b713ac08c6cc22
Luca <Luca@10.100.11.243>
2013-12-30 17:49:20 +0100

    Edit Task lato client e lato server

--- src/asw1009/viewmodel/request/EditTaskRequestViewModel.java
+++ src/asw1009/viewmodel/request/EditTaskRequestViewModel.java
@@ -3,24 +3,122 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package asw1009.viewmodel.request;
 
-import asw1009.model.entities.Task;
-
+import java.util.Date;
 
 /**
  *
  * @author Andrea
  */
 public class EditTaskRequestViewModel {
-    private Task task;
-    
-    public Task getTask(){
-        return this.task;
-    }
-    
-    public void setTask(Task task){
-        this.task = task;
-    }
+
+	private int id;
+	private String title;
+	private String description;
+	private Date date;
+	private boolean done;
+	private int categoryId;
+	private int userId;
+	private double latitude;
+	private double longitude;
+	private boolean personal;
+	private int assignedUserId;
+	private String attachment;
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public int getUserId() {
+		return this.userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getTitle() {
+		return this.title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return this.description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getDate() {
+		return this.date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public boolean getDone() {
+		return this.done;
+	}
+
+	public void setDone(boolean done) {
+		this.done = done;
+	}
+
+	public int getCategoryId() {
+		return this.categoryId;
+	}
+
+	public void setCategoryId(int categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public double getLatitude() {
+		return this.latitude;
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+	public double getLongitude() {
+		return this.longitude;
+	}
+
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+
+	public String getAttachment() {
+		return this.attachment;
+	}
+
+	public void setAttachment(String attachment) {
+		this.attachment = attachment;
+	}
+
+	public int getAssignedUserId() {
+		return this.assignedUserId;
+	}
+
+	public void setAssignedUserId(int assignedUserId) {
+		this.assignedUserId = assignedUserId;
+	}
+
+	public boolean getPersonal() {
+		return this.personal;
+	}
+
+	public void setPersonal(boolean personal) {
+		this.personal = personal;
+	}
 }
