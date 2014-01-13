1fb28ca67e6e246d7b9c0a32abbe60ede5aa567a
Luca <Luca@10.100.11.243>
2014-01-13 23:27:56 +0100

    aggiunto il campo remember alla loginrequestviewmodel

--- src/asw1009/viewmodel/request/LoginRequestViewModel.java
+++ src/asw1009/viewmodel/request/LoginRequestViewModel.java
@@ -1,22 +1,32 @@
 package asw1009.viewmodel.request;
 
 public class LoginRequestViewModel {
+
     private String username;
     private String password;
-    
-    public String getUsername(){
+    private String remember;
+
+    public String getUsername() {
         return this.username;
     }
-    
-    public void setUsername(String username){
+
+    public void setUsername(String username) {
         this.username = username;
     }
-    
-    public String getPassword(){
+
+    public String getPassword() {
         return this.password;
     }
-    
-    public void setPassword(String password){
+
+    public void setPassword(String password) {
         this.password = password;
+    }
+
+    public void setRemember(String remember) {
+        this.remember = remember;
+    }
+
+    public String getRemember() {
+        return this.remember;
     }
 }
